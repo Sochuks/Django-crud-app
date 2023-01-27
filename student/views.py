7d56a078f2f70a8c7d1c7a0f9d875505768e79b1
Sochuks <moseschukwumakajang@gmail.com>
2023-01-27 12:34:37 +0100

    added nav and table with students records

--- student/views.py
+++ student/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import stdnt
 
 # Create your views here.
 def index(request):
-    return render(request, "student/index.html")
+    return render(request, "student/index.html", {
+        'students': stdnt.objects.all()
+    })
+    
