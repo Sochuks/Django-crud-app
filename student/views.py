5cce91d3f8754f1e5956191f6a952cb017e9f506
Sochuks <moseschukwumakajang@gmail.com>
2023-01-28 14:44:31 +0100

    create view details using modal display

--- student/views.py
+++ student/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from .models import stdnt
 
 # Create your views here.
@@ -6,4 +8,9 @@ def index(request):
     return render(request, "student/index.html", {
         'students': stdnt.objects.all()
     })
+
+# View students 
+def view_student(request, id):
+    student = stdnt.objects.get(pk=id)
+    return HttpResponseRedirect(reverse('index'))
     
