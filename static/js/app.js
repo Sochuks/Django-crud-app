94539db051743cb026a9a3839df74b9a7babcb3e
Sochuks <moseschukwumakajang@gmail.com>
2023-02-01 13:46:55 +0100

    compressor

---
+++ static/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('#display_table').DataTable({
+    "aLengthMenu": [[3, 5, 10, 25, -1], [3, 5, 10, 25, "All"]],
+      "iDisplayLength": 3
+     }
+  );
+} )
+
+
+alert("stew")
